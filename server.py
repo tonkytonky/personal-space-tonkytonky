6197c46d297fd4730145f75548ff9a392a9294bb
tonkytonky <as@asamsonov.me>
2019-02-24 11:41:10 +0300

    Initial commit

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/job.html')
+def job():
+    return render_template('job.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
